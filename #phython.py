75cae2e37de2db5049571b052e95f0d8e5aefdd6
DaudNasir1 <daudnasir5159@gmail.com>
2024-09-15 00:23:06 +0500

    Create #phython.py

---
+++ #phython.py
@@ -0,0 +1,37 @@
+#phython
+# string
+name = "Alice"
+greeting = 'Hello, ' + name + '!'
+print(greeting) 
+# f_string 
+age = 25
+message = f'{name} is {age} years old.'
+print(message)
+# operates
+x = 10
+y = 3
+
+# Arithmetic
+sum_result = x + y      
+power_result = x ** y  
+is_equal = x == y        # False
+is_greater = x > y       # True
+
+
+logical_result = (x > y) and (y > 0) 
+print(logical_result)
+# list
+ examplfruits = ["apple", "banana", "cherry"]
+fruits.append("orange")   
+print(fruits[1])          
+print(len(fruits))       
+# tuples
+point = (5, 10)
+x, y = point  
+print(x, y)   
+# for_loops
+for fruit in fruits:(apple,banana,cherry,orange,)
+print(fruit)
+# input_handling
+user_name = input("Enter your name: ")
+print(f"Hello, {user_name}!")
